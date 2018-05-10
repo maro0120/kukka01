577d757192e82421516ebf17da77013a35f7dd81
maro0120 <37236059+maro0120@users.noreply.github.com>
2018-05-10 09:51:31 +0900

    Add files via upload

--- kukka/assets/js/main.js
+++ kukka/assets/js/main.js
@@ -27,7 +27,28 @@
 		$('#nav-toggle,#global-nav ul li a').click(function(){
 		    $header.toggleClass('open');
 		});
+		$('a[href ^= "#"]').click(function() {
+        $('html,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        	}, 300);
+		})
     });
+
+	$(function() {
+		var startPos = 0,winScrollTop = 0;
+		$(window).on('scroll',function(){
+		    winScrollTop = $(this).scrollTop();
+		    if (winScrollTop >= startPos) {
+		if(winScrollTop >= 200){
+			$('.site-header').addClass('hide');
+		}
+		    } else {
+		        $('.site-header').removeClass('hide');
+		    }
+		    startPos = winScrollTop;
+		});
+	});
+
 
 	$(function() {
 
